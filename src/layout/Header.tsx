cce57961b9e5736c360a90f8cddc7a65b9fa5a8a
Lucas <lucas.s.augusto@hotmail.com>
2026-02-10 14:47:23 -0300

    feat(Header): 🚧 Start a MVP - Header Title

---
+++ src/layout/Header.tsx
@@ -0,0 +1,10 @@
+import './Header.css';
+
+export function Header() {
+  return (
+    <div className="main-header">
+      <h1 className="main-title">Rush Chess</h1>
+      <p className="main-sub-title">Battle with your friends rushing against time</p>
+    </div>
+  )
+}
